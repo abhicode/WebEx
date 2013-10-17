6e1ea9d56bafe8e7e6edec5e986522592cc2ae84
abhicode <abhishek.gupta890@gmail.com>
2013-10-17 23:37:22 +0530

    Update OsInfo.js
    Node.js Require 'OS' Module With different functions and properties

--- OsInfo.js
+++ OsInfo.js
@@ -6,10 +6,43 @@ console.log("No of CPU's (or Number of Cores) :" + cpusLen);
 var totalMemory = osObj.totalmem();
 console.log("Total PhysicalMemory:" + totalMemory);
 
-var networkInterfaces = osObj.networkinterfaces();
+var networkInterfaces = osObj.networkInterfaces();
 console.log("Network Interfaces List on this system "+networkInterfaces);
 
 
 var currentOsArchitecture = osObj.arch();
-console.log("Current OS Architecture");
+console.log("Current OS Architecture" + currentOsArchitecture);
+
+var currentOsEndOfLine = osObj.EOL;
+console.log("Current OS End Of line" + currentOsEndOfLine);
+
+var currentOsEndiannes = osObj.endianness();
+console.log("Current SYstem Endianness type" + currentOsEndinness);
+
+var freeMem = osObj.freemem();
+console.log("Current Total Free Physical Mem : "+freeMem);
+
+var hostname = osObj.hostname();
+console.log("System Host Name : " + hostname);
+
+var loadAvg = osObj.loadavg();
+console.log("Current Avg Load : " + loadAvg);
+
+var platform = osObj.platform();
+console.log("Current System OS Platform : "+platform);
+
+var release = osObj.release();
+console.log("OS Release Version : " + release);
+
+var tempdir = osObj.tmpdir();
+console.log("Current temporary Dir : "+tempdir);
+
+var osType = osObj.type();
+console.log("OS Type  : " + osType);
+
+var uptime = osObj.uptime();
+console.log("System Up Time : "+uptime + "Seconds");
+
+
+
 
