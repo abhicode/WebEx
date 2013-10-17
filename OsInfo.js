9a3452254091765db71375c1d324fcb0633d1130
abhicode <abhishek.gupta890@gmail.com>
2013-10-17 23:18:09 +0530

    Update OsInfo.js
    Usage of 'OS' Module in Node.js

--- OsInfo.js
+++ OsInfo.js
@@ -1,3 +1,15 @@
 var osObj=require('os');
+
 var cpusLen = osObj.cpus().length;
+console.log("No of CPU's (or Number of Cores) :" + cpusLen);
+
+var totalMemory = osObj.totalmem();
+console.log("Total PhysicalMemory:" + totalMemory);
+
+var networkInterfaces = osObj.networkinterfaces();
+console.log("Network Interfaces List on this system "+networkInterfaces);
+
+
+var currentOsArchitecture = osObj.arch();
+console.log("Current OS Architecture");
 
